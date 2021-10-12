513282eba16c4e907630ca50b8b0698d168e3e0b
rei <mizukami-r@kkhts.com>
2021-10-13 07:56:03 +0900

    Apply paging to result table.

--- src/components/DataViewTable.tsx
+++ src/components/DataViewTable.tsx
@@ -5,13 +5,26 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
 } from "@mui/material";
+import { ChangeEvent, useState } from "react";
 import { useDummyData } from "../context/DummyDataProvider";
 import Title from "./Title";
 
 const DataViewTable = () => {
   const { dummyDataSet } = useDummyData();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(Number(event.target.value));
+    setPage(0);
+  };
 
   return (
     <Paper elevation={3} sx={{ p: 2 }}>
@@ -21,27 +34,49 @@ const DataViewTable = () => {
           <TableHead>
             <TableRow>
               {dummyDataSet?.columnPropsList.map(({ id, name }) => (
-                <TableCell key={id} align="center">
+                <TableCell
+                  key={id}
+                  align="center"
+                  sx={{
+                    backgroundColor: "primary.main",
+                    color: "primary.contrastText",
+                  }}
+                >
                   {name}
                 </TableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {dummyDataSet?.records.map((dataRecord) => (
-              <TableRow key={dataRecord.id}>
-                {dummyDataSet?.columnPropsList.map((columnProp) => (
-                  <TableCell key={columnProp.id} align="center">
-                    {dataRecord.record.find(
-                      (dataProps) => dataProps.id === columnProp.id
-                    )?.data || ""}
-                  </TableCell>
-                ))}
-              </TableRow>
-            )) || ""}
+            {dummyDataSet?.records
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((dataRecord) => (
+                <TableRow key={dataRecord.id}>
+                  {dummyDataSet?.columnPropsList.map((columnProp) => (
+                    <TableCell key={columnProp.id} align="center">
+                      {dataRecord.record.find(
+                        (dataProps) => dataProps.id === columnProp.id
+                      )?.data || ""}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              )) || ""}
           </TableBody>
         </Table>
       </TableContainer>
+      {dummyDataSet ? (
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={dummyDataSet.records.length}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      ) : (
+        ""
+      )}
     </Paper>
   );
 };
